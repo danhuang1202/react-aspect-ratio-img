ae51f0fb19f919f9ee60dfb52840c89c66631330
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-26 14:09:58 +0000

    Deploy danhuang1202/react-aspect-ratio-img to github.com/danhuang1202/react-aspect-ratio-img.git:gh-pages

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import AspectRatioImg from './components/AspectRatioImg'
-import AspectRatioImgLegacy from './components/AspectRatioImgLegacy'
-import SquareImg from './components/SquareImg'
-
-export { AspectRatioImg, AspectRatioImgLegacy, SquareImg }
